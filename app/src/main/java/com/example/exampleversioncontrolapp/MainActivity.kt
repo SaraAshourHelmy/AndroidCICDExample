8d3e7b2d2f1169e6a40116ba2b8ed6925746d204
SASHOUR <sara.ashou@volvocars.com>
2024-01-04 12:57:15 +0200

    add a new button

--- app/src/main/java/com/example/exampleversioncontrolapp/MainActivity.kt
+++ app/src/main/java/com/example/exampleversioncontrolapp/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.exampleversioncontrolapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.exampleversioncontrolapp.ui.theme.ExampleVersionControlAppTheme
 
@@ -18,11 +22,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             ExampleVersionControlAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    //color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("CI/CD Android")
+                    Button(onClick = {
+                        Toast.makeText(this@MainActivity, "Hi Sara", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Text(
+                            text = "Click me",
+                        )
+                    }
                 }
             }
         }
